0655049e51416ccd4f40b79cca86e9472be0dfa0
scivision <scivision@users.noreply.github.com>
2017-02-08 12:17:38 -0500

    test

--- tests/test.py
+++ tests/test.py
@@ -5,10 +5,19 @@ from numpy.testing import run_module_suite,assert_allclose
 #
 import sciencedates as sd
 
-def test_fortrandates():
+def test_yearint():
+    adatetime=datetime(2013,7,2,12,0,0,tzinfo=UTC)
+    yd,utsec = sd.datetime2yd(adatetime)
+
+    utsec2 = sd.dt2utsec(adatetime)
+
+    assert sd.yd2datetime(yd,utsec) == adatetime
+    assert utsec==utsec2
+
+def test_yeardec():
     adatetime=datetime(2013,7,2,12,0,0,tzinfo=UTC)
     yeardec = sd.datetime2yeardec(adatetime)
-    
+
     assert_allclose(yeardec,2013.5)
     assert sd.yeardec2datetime(yeardec) == adatetime
 
@@ -16,14 +25,14 @@ def test_utc():
     adatetime=datetime(2013,7,2,12,0,0)
     estdt = timezone('EST').localize(adatetime)
     utcdt = sd.forceutc(estdt)
-    
+
     assert utcdt==estdt
     assert utcdt.tzname()=='UTC'
 
-def test_datetimefortran():
+def test_gtd():
     adatetime=datetime(2013,7,2,12,0,0)
     iyd,utsec,stl= sd.datetime2gtd(adatetime,glon=42)
-    
+
     assert iyd[0]==183
     assert_allclose(utsec[0],43200)
     assert_allclose(stl[0],14.8)
