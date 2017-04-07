c835a32122270481a274f34695315467e0b8f303
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-07 00:43:48 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ req = ['nose','numpy','pytz','python-dateutil','xarray','matplotlib']
 
 setup(name='sciencedates',
       packages=['sciencedates'],
-      version = '1.2.2',
+      version = '1.2.3',
       description='Date conversions used in the sciences.',
       author = 'Michael Hirsch, Ph.D.',
       url = 'https://github.com/scivision/sciencedates',
