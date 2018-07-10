55c99939b67404a480a191a6f47ff76844b808d4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-10 17:49:22 -0400

    in

---
+++ findnearest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import numpy as np
+from bisect import bisect
+import sciencedates as sd
+
+
+def INCORRECTRESULT_using_bisect(x, X0):  # pragma: no cover
+    X0 = np.atleast_1d(X0)
+    x.sort()
+    ind = [bisect(x, x0) for x0 in X0]
+
+    x = np.asanyarray(x)
+    return np.asanyarray(ind), x[ind]
+
+
+def main():
+    print(sd.find_nearest([10, 15, 12, 20, 14, 33], [32, 12.01]))
+
+    print(INCORRECTRESULT_using_bisect([10, 15, 12, 20, 14, 33], [32, 12.01]))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
